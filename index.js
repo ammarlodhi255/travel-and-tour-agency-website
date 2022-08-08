3b1c2989a799b17add93937d8bf5efab5a23975c
ammarlodhi255 <ammarlodhi68@gmail.com>
2022-08-08 23:32:25 +0500

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const loginBtn = document.querySelector('.login');
     loginBtn.addEventListener('click', () => {
-
+        loginBtn.setAttribute('href', './login.html');
     });
 }
