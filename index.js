e2ff99f844add20a7d5354ff7b6d10af9dd71c6e
Abdul Manaf <sahitoa97@gmail.com>
2022-08-13 02:03:19 +0500

    navigation links fix

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.onload = () => {
     const loginBtn = document.querySelector('.login');
+    const signupBtn = document.querySelector('.signup');
     const closeBtn = document.querySelector('.close-btn');
     loginBtn.addEventListener('click', () => {
         loginBtn.setAttribute('href', './login.html');
     });
-
-    
+    signupBtn.addEventListener('click', () => {
+        signupBtn.setAttribute('href', './signup.html');
+    });
 }
 
