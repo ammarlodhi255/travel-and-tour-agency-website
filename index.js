6c476150af719ccbca6deea8b5b28ce3db8653c6
Abdul Manaf <sahitoa97@gmail.com>
2022-08-13 02:21:04 +0500

    about page added and links updated.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 window.onload = () => {
     const loginBtn = document.querySelector('.login');
     const signupBtn = document.querySelector('.signup');
+    const aboutBtn = document.querySelector('.about');
     const closeBtn = document.querySelector('.close-btn');
     loginBtn.addEventListener('click', () => {
         loginBtn.setAttribute('href', './login.html');
     });
     signupBtn.addEventListener('click', () => {
         signupBtn.setAttribute('href', './signup.html');
+    });
+    aboutBtn.addEventListener('click', () => {
+        aboutBtn.setAttribute('href', './about.html');
+        console.log('mdjnj');
     });
 }
 
