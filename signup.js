4781b07fa70d84ecbd24b3b2cdb58a4d76c56f29
Abdul Manaf <sahitoa97@gmail.com>
2022-08-14 20:41:52 +0500

    signup backhend functionality

--- signup.js
+++ signup.js
@@ -1,10 +1,29 @@
 let signupBtn = document.querySelector("button.signup");
 signupBtn.onclick = () => {
-  let name = document.querySelector("#name").value;
-  let email = document.querySelector("#email").value;
-  let phone = document.querySelector("#phone").value;
-  let password = document.querySelector("#password").value;
+  let name = document.querySelector("#name");
+  let email = document.querySelector("#email");
+  let phone = document.querySelector("#phone");
+  let password = document.querySelector("#password");
 
-  console.log(name + " : " + phone);
-  console.log(email + " : " + password);
+  let new_user = {
+    Picture: "",
+    Name: name.value,
+    Email: email.value,
+    Phone: phone.value,
+    Password: password.value,
+  }
+  let users = JSON.parse(localStorage.getItem("Users"));
+  users.push(new_user);
+  localStorage.setItem("Users", JSON.stringify(users));
+
+  let label = document.createElement("label");
+  label.innerText = "User successfully added";
+  label.style.color = "green";
+  label.style.margin = "5em";
+  signupBtn.parentElement.appendChild(label);
+
+  name.value = "";
+  email.value = "";
+  phone.value = "";
+  password.value = "";
 };
